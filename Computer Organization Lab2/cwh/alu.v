17c46d7a297e2130231e1913be805a0d479f0b27
eazyreal <0312fs3@gmail.com>
2020-04-20 17:55:36 +0800

    ALU doing, testbench.v passed

--- Computer Organization Lab2/cwh/alu.v
+++ Computer Organization Lab2/cwh/alu.v
@@ -5,7 +5,6 @@ module alu(
            src1,          // 32 bits source 1          (input)
            src2,          // 32 bits source 2          (input)
            ALU_control,   // 4 bits ALU control input  (input)
-		  bonus_control, // 3 bits bonus control input(input) 
            result,        // 32 bits result            (output)
            zero,          // 1 bit when the output is 0, zero must be set (output)
            cout,          // 1 bit carry out           (output)
@@ -17,34 +16,38 @@ input           rst_n;
 input  [32-1:0] src1;
 input  [32-1:0] src2;
 input   [4-1:0] ALU_control;
-// input   [3-1:0] bonus_control; 
 
 output [32-1:0] result;
 output          zero;
 output          cout;
 output          overflow;
 
-wire             zero;
-wire             cout;
-wire             overflow;
+wire            zero;
+wire            cout;
+wire            overflow;
+
+//for slt 
+wire            eq31;
+wire            lt;
+wire            set;
+
 
 reg [1:0] oper;
 wire [31:0] carry;
 reg a_in;
 reg b_in;
 reg less_sig;
-wire			set;
-wire			equal;
 
-assign carry[0] = (ALU_control==4'b0110)? 1: (ALU_control==4'b0111)? 1: 0; //sub slt: cin =1
+//ZCY flag 
+assign carry[0] = (ALU_control==4'b0110)? 1: (ALU_control==4'b0111)? 1: 0; //sub or slt, cin = 1
 assign zero = (result == 0) ? 1 : 0;
-//assign overflow = carry[31] ^ cout;
-assign equal = (src1 == src2) ? 1 : 0;
-assign overflow = ( (ALU_control==4'b0000) & src1[31] & src2[31] & ~result[31]) ? 1 
-					  :( (ALU_control==4'b0000) & ~src1[31] & ~src2[31] & result[31]) ? 1 
-					  :( (ALU_control==4'b0110) & src1[31] & ~src2[31] & ~(result[31])) ? 1 
-					  :( (ALU_control==4'b0110) & ~src1[31] & src2[31] & result[31]) ? 1 
-					  : 0;
+assign overflow = carry[31] ^ cout;
+
+//slt judge
+assign eq31 = src1[31] ^ ~src2[31]; //a, -b same sign or not
+assign lt = eq31 ? carry[31]^1 : src1[31]; //same => no overflow, not => use a sign(avoid overflow)
+assign set = 0;
+//assign res = less_sig ? (lt ? 1 : 0) : res;
 
 always@(*)begin
 	if(rst_n==1)begin
@@ -53,37 +56,37 @@ always@(*)begin
 		case(ALU_control)
 			4'b0000:begin//And
 					a_in  	<= 0;
-					b_in 	 	<= 0;
+					b_in 	<= 0;
 					oper  	<= 2'b00;//and
 					end
 			4'b0001:begin//Or
 					a_in  	<= 0;
-					b_in 	 	<= 0;
+					b_in 	<= 0;
 					oper  	<= 2'b01;//or
 					end
 			4'b0010:begin//Add
 					a_in  	<= 0;
-					b_in 	 	<= 0;
+					b_in 	<= 0;
 					oper  	<= 2'b10;//add
 					end
 			4'b0110:begin//Sub
 					a_in  	<= 0;
-					b_in 	 	<= 1;
+					b_in 	<= 1;
 					oper  	<= 2'b10;//add
 					end
 			4'b1100:begin//Nor
 					a_in  	<= 1;
-					b_in 	 	<= 1;
+					b_in 	<= 1;
 					oper  	<= 2'b00;//and
 					end
 			4'b1101:begin//Nand
 					a_in  	<= 1;
-					b_in 	 	<= 1;
+					b_in 	<= 1;
 					oper  	<= 2'b01;//or
 					end
 			4'b0111:begin//SetLessThan
 					a_in  	<= 0;
-					b_in 	 	<= 1;
+					b_in 	<= 1;
 					oper  	<= 2'b11;//less
 					end
 			default: ;
@@ -91,19 +94,23 @@ always@(*)begin
 	end
 end
 
+//diff lt
+alu_last alu0( .src1(src1[0]), .src2(src2[0]), .less(lt), .A_invert(a_in), .B_invert(b_in),
+					.cin(carry[0]), .operation(oper), .result(result[0]), .cout(carry[1]) );
+
 parameter NBIT = 30;
 generate
 genvar i;
-for (i=0; i<=NBIT; i=i+1)
-begin: u
+for (i=1; i<=NBIT; i=i+1)
+begin: 
 alu_top alui( .src1(src1[i]), .src2(src2[i]), .less(set), .A_invert(a_in), .B_invert(b_in),
 				  .cin(carry[i]), .operation(oper), .result(result[i]), .cout(carry[i+1]) );
 end
 endgenerate 
 
-alu_last alu31( .src1(src1[31]), .src2(src2[31]), .less(less_sig), .A_invert(a_in), .B_invert(b_in),
-					.cin(carry[31]), .operation(oper), .result(result[31]), .cout(cout),
-					.set(set), .equal(equal), .cmp(bonus_control) );
+//diff cout
+alu_last alu31( .src1(src1[31]), .src2(src2[31]), .less(set), .A_invert(a_in), .B_invert(b_in),
+					.cin(carry[31]), .operation(oper), .result(result[31]), .cout(cout) );
 
 
 endmodule
--- Computer Organization Lab2/cwh/alu_last.v
+++ Computer Organization Lab2/cwh/alu_last.v
@@ -29,9 +29,6 @@ module alu_last(
                operation,  //operation      (input)
                result,     //1 bit result   (output)
                cout,       //1 bit carry out(output)
-					set,
-					equal,
-					cmp	//bonus
                );
 
 input         src1;
@@ -41,29 +38,16 @@ input         A_invert;
 input         B_invert;
 input         cin;
 input [2-1:0] operation;
-input			  equal;
-input [2:0]   cmp;
-
 
 output   reg  result;
 output   reg   cout;
-output		set;
 
 reg s1, s2;
 reg done;
-//assign set = s1 ^ s2 ^ cin;//set_basic
-//set_bonus
-assign	set = (cmp==3'b000) ? (s1 ^ s2 ^ cin) : //set less than <
-					(cmp==3'b001) ? ~( (s1 ^ s2 ^ cin) | equal) : //set great than >
-					(cmp==3'b010) ? ((s1 ^ s2 ^ cin) | equal) : //set less equal <=
-					(cmp==3'b011) ? (~ (s1 ^ s2 ^ cin)) ://set great equal >=
-					(cmp==3'b110) ? (equal) ://set equal ==
-					(cmp==3'b100) ? (~equal) : 0;//set not equal !=
-
 
 always@( * )begin
 
-done =0 ;
+done = 0;
 
 if(done == 0)begin
 	if(A_invert) s1 = ~src1;	else s1 = src1;
@@ -72,11 +56,13 @@ if(done == 0)begin
 	case(operation)
 		2'b00:begin//And
 			result = s1 & s2;
-			cout = 0;end
+			cout = 0;
+            end
 			
 		2'b01:begin//Or
 			result = s1 | s2;
-			cout = 0;end
+			cout = 0;
+            end
 		
 		2'b10:begin//Add
 			result = s1 ^ s2 ^ cin;
@@ -85,8 +71,7 @@ if(done == 0)begin
 		
 		2'b11:begin//less
 			result = less;
-			cout = 0; //SLT do not have cout!
-			//cout = (s1&s2) + (s1&cin) + (s2&cin); 
+			cout = 0;
 			end
 	endcase
 	
